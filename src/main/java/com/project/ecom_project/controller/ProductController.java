81c3d2c5d879adc41815a2ff2959b98c855555f4
trilok <trilokkambham2001@gmail.com>
2025-06-28 11:10:04 -0500

    service and repo files created for the DB data flow.

--- src/main/java/com/project/ecom_project/controller/ProductController.java
+++ src/main/java/com/project/ecom_project/controller/ProductController.java
@@ -1,15 +1,29 @@
 package com.project.ecom_project.controller;
 
+import com.project.ecom_project.model.Product;
+import com.project.ecom_project.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
 
+    @Autowired
+    private ProductService service;
+
     @RequestMapping("/")
     public String greet(){
         return "Hello World";
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts(){
+        return service.getAllProducts();
     }
 }
---
+++ src/main/java/com/project/ecom_project/repo/ProductRepo.java
@@ -0,0 +1,9 @@
+package com.project.ecom_project.repo;
+
+import com.project.ecom_project.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+}
