544ed20e6744cf50fa13e7969ae0a3b13eb0d262
trilok <trilokkambham2001@gmail.com>
2025-06-28 19:36:28 -0500

    Connection between frontend and backend made successfully.

--- src/main/java/com/project/ecom_project/controller/ProductController.java
+++ src/main/java/com/project/ecom_project/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.project.ecom_project.model.Product;
 import com.project.ecom_project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
 
